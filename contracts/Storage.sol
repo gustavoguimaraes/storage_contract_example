ba18f025c01c71d789e59ddc686adae90c5baf68
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2017-05-16 12:52:05 +0200

    Add spaces to contract so it is easier to read.

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,9 +1,12 @@
 pragma solidity ^0.4.8;
+
 contract Storage {
   uint256 storedData;
+
   function set(uint256 data) {
     storedData = data;
   }
+
   function get() constant returns (uint256) {
     return storedData;
   }
